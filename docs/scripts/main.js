9f3be680a26e7f680b33205a5971074c853b225e
pduerst <peterad12@gmail.com>
2020-03-08 21:03:30 -0400

    Compatible with one-way communication.

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -1,3 +1,11 @@
+const url = 'http://localhost:8080';
+
+$(document).ready(function(){
+  $("p").click(function(){
+    $(this).hide();
+  });
+});
+
 function convertFormToJson(form) {
   var array = jQuery(form).serializeArray();
   var json = {};
@@ -9,11 +17,9 @@ function convertFormToJson(form) {
   return json;
 }
 
-$(document).ready(function(){
-  $("p").click(function(){
-    $(this).hide();
-  });
-});
+const handleResponse = (data, status) => {
+  responseField.innerHTML = `<p>You've submitted a response!<br>Status: ${status}<br>Response: ${data.message}</p>`;
+}
 
 $(document).ready(function(){
   $("form").submit(function(event){
@@ -21,15 +27,17 @@ $(document).ready(function(){
     
     var form = this;
     var json = convertFormToJson(form);
-    
-    var time_sent = new Date().toString();
-    json.time_sent = time_sent;
-    
-    $.post(
-      "http://localhost:5000/",
-      json,
-      function(data,status){
-        responseField.innerHTML = `<p>You've submitted a response!<br>Status: ${status}<br>Response: ${data.response}</p>`;
-    });
+    var query = `?command=${json.command}&val1=${json.val1}&val2=${json.val2}`;
+    // responseField.innerHTML = `<p>Query: ${query}</p>`;
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', `${url}/${query}`, true);
+    xhr.onreadystatechange = function () {
+      responseField.innerHTML = `<p>Response: ${xhr.readyState}, Status: ${xhr.status}</p>`;
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        responseField.innerHTML = `<p>Response: ${xhr.responseText}</p>`;
+      }
+    };
+    xhr.send();
+    // $.get(`${url}/${query}`, function(data){alert(`Data: ${data}`);});
   });
 });
