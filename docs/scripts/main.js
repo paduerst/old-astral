3e25cd28dd04ebc76ed2926532704a738420deb1
pduerst <peterad12@gmail.com>
2020-03-09 19:53:25 -0400

    Minor edits. Cleaned up code.

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -1,30 +1,21 @@
-let url = 'http://localhost:8080';
-
-function updateUrl(json) {
-  const subdomain = json.subdomain;
-  const rootdomain = json.rootdomain;
-  url = `https://${subdomain}.${rootdomain}`;
-  urlField.innerHTML = `Url set to: ${url}`;
-}
-
-function resetUrlField() {
-
-}
+const defaultUrl = 'http://localhost:8080';
+let url = defaultUrl;
 
 function convertFormToJson(form) {
   var array = jQuery(form).serializeArray();
   var json = {};
   json["command"] = form.name;
-  
   jQuery.each(array, function() {
     json[this.name] = this.value || '';
   });
-  
   return json;
 }
 
-const handleResponse = (data, status) => {
-  responseField.innerHTML = `<p>You've submitted a response!<br>Status: ${status}<br>Response: ${data.message}</p>`;
+function updateUrl(json) {
+  const subdomain = json.subdomain;
+  const rootdomain = json.rootdomain;
+  url = `https://${subdomain}.${rootdomain}`;
+  urlField.innerHTML = `<h4>Submitting to: ${url}</h4>`;
 }
 
 $(document).ready(function(){
@@ -34,14 +25,12 @@ $(document).ready(function(){
     if (form.id === "form_external") {
       var json = convertFormToJson(form);
       var query = `?command=${json.command}&val1=${json.val1}&val2=${json.val2}`;
-      outputField.innerHTML = `<p>Output: ${url}/${query}</p>`;
-      // responseField.innerHTML = `<p>Query: ${query}</p>`;
+      outputField.innerHTML = `<p>Last submission: ${url}/${query}</p>`;
       var xhr = new XMLHttpRequest();
       xhr.open('GET', `${url}/${query}`, true);
       xhr.onreadystatechange = function () {
-        responseField.innerHTML = `<p>Response: ${xhr.readyState}, Status: ${xhr.status}</p>`;
         if (xhr.readyState === 4 && xhr.status === 200) {
-          responseField.innerHTML = `<p>Response: ${xhr.responseText}</p>`;
+          alert(`Status 200?!This is unexpected!`);
         }
       };
       xhr.send();
